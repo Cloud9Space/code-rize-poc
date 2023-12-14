ebc4da6f2cbf2d9af30413672548cd442372b4f5
Siddhant Nawale <siddhant.nawale@gmail.com>
2023-12-15 00:48:09 +0530

    Created LayerControl module

--- src/Map.js
+++ src/Map.js
@@ -6,17 +6,16 @@ import 'leaflet/dist/leaflet.css';
 import geojson from 'json!./Guija_Settelments.geojson';
 import polulation from 'json!./Guija_population.geojson';
 // import local components Filter and ForkMe
-import Filter from './Filter';
-import ForkMe from './ForkMe';
+
 import 'leaflet-draw/dist/leaflet.draw.js';
 import 'leaflet-draw/dist/leaflet.draw.css';
 // store the map configuration properties in an object,
 // we could also move this to a separate file & import it if desired.
 let config = {};
 config.params = {
-  center: [40.655769, -73.938503],
+  center: [-24.749434, 32.961285],
   zoomControl: false,
-  zoom: 13,
+  zoom: 8,
   scrollwheel: false,
   legends: true,
   infoControl: false,
@@ -34,28 +33,39 @@ config.tileLayer = {
   }
 };
 
-// array to store unique names of Brooklyn subway lines,
-// this eventually gets passed down to the Filter component
-let subwayLineNames = [];
+
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '© OpenStreetMap'
+});
+
+var osmHOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France'
+});
+
+var baseMaps= {
+  "OpenStreetMap": osm,
+  "OpenStreetMap.HOT": osmHOT
+}
+var overlayMaps = {
+  
+}
+
+
+
 
 class Map extends Component {
   constructor(props) {
     super(props);
     this.state = {
       map: null,
-      tileLayer: null,
-      geojsonLayer: null,
-      polulationgeojson: null,
-      geojson: null,
-      // subwayLinesFilter: '*',
-      numEntrances: null
+      // tileLayer: null,
+      // geojsonLayer: null,
+      // polulationgeojson: null,
+      // geojson: null,
     };
     this._mapNode = null;
-    // this.updateMap = this.updateMap.bind(this);
-    this.onEachFeature = this.onEachFeature.bind(this);
-    this.pointToLayer = this.pointToLayer.bind(this);
-    // this.filterFeatures = this.filterFeatures.bind(this);
-    this.filterGeoJSONLayer = this.filterGeoJSONLayer.bind(this);
   }
 
   componentDidMount() {
@@ -66,21 +76,21 @@ class Map extends Component {
     if (!this.state.map) this.init(this._mapNode);
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    // code to run when the component receives new props or state
-    // check to see if geojson is stored, map is created, and geojson overlay needs to be added
-    if (this.state.geojson && this.state.map && !this.state.geojsonLayer) {
-      // add the geojson overlay
-      this.addGeoJSONLayer(this.state.geojson);
-      this.addGeoJSONLayer(this.state.polulationgeojson);
-    }
+  // componentDidUpdate(prevProps, prevState) {
+  //   // code to run when the component receives new props or state
+  //   // check to see if geojson is stored, map is created, and geojson overlay needs to be added
+  //   if (this.state.geojson && this.state.map && !this.state.geojsonLayer) {
+  //     // add the geojson overlay
+  //     this.addGeoJSONLayer(this.state.geojson);
+  //     this.addGeoJSONLayer(this.state.polulationgeojson);
+  //   }
 
-    // check to see if the subway lines filter has changed
-    if (this.state.subwayLinesFilter !== prevState.subwayLinesFilter) {
-      // filter / re-render the geojson overlay
-      this.filterGeoJSONLayer();
-    }
-  }
+  //   // check to see if the subway lines filter has changed
+  //   if (this.state.subwayLinesFilter !== prevState.subwayLinesFilter) {
+  //     // filter / re-render the geojson overlay
+  //     this.filterGeoJSONLayer();
+  //   }
+  // }
 
   componentWillUnmount() {
     // code to run just before unmounting the component
@@ -92,34 +102,20 @@ class Map extends Component {
     // could also be an AJAX request that results in setting state with the geojson data
     // for simplicity sake we are just importing the geojson data using webpack's json loader
     this.setState({
-      numEntrances: geojson.features.length,
       geojson
     });
   }
 
-  // updateMap(e) {
-  //   let subwayLine = e.target.value;
-  //   // change the subway line filter
-  //   if (subwayLine === "All lines") {
-  //     subwayLine = "*";
-  //   }
-  //   // update our state with the new filter value
-  //   this.setState({
-  //     subwayLinesFilter: subwayLine
-  //   });
-  // }
 
 
-
-  addGeoJSONLayer(geojson) {
+  addGeoJSONLayer() {
     // create a native Leaflet GeoJSON SVG Layer to add as an interactive overlay to the map
     // an options object is passed to define functions for customizing the layer
-    const geojsonLayer = L.geoJson(geojson, {
+    const settlementLayer = L.geoJson(geojson, {
       // onEachFeature: this.onEachFeature,
       // pointToLayer: this.pointToLayer,
       // filter: this.filterFeatures
       style: function (feature) {
-        // Customize the style based on feature properties if needed
         return {
           color: '#CC0066',
           fillColor: '#CC0066',
@@ -130,10 +126,12 @@ class Map extends Component {
       }
     });
     // add our GeoJSON layer to the Leaflet map object
-    geojsonLayer.addTo(this.state.map);
-    this.setState({ geojsonLayer });
+    // geojsonLayer.addTo(this.state.map);
+    // overlayMaps['settlement'] = geojsonLayer
+    overlayMaps['settlement'] = settlementLayer
+    this.setState({ settlementLayer });
 
-    const geojsonLayer2 = L.geoJson(polulation, {
+    const populationLayer = L.geoJson(polulation, {
       // onEachFeature: this.onEachFeature,
       // pointToLayer: this.pointToLayer,
       // filter: this.filterFeatures
@@ -149,96 +147,28 @@ class Map extends Component {
       }
     });
     // add our GeoJSON layer to the Leaflet map object
-    geojsonLayer2.addTo(this.state.map);
-    this.setState({ polulationgeojson: geojsonLayer2 });
+    // geojsonLayer2.addTo(this.state.map);
+    overlayMaps['population'] = populationLayer
 
+    this.setState({ polulationgeojson: populationLayer });
 
-    // store the Leaflet GeoJSON layer in our component state for use later
-    // fit the geographic extent of the GeoJSON layer within the map's bounds / viewport
-    this.zoomToFeature(geojsonLayer);
   }
 
-  filterGeoJSONLayer() {
-    // clear the geojson layer of its data
-    this.state.geojsonLayer.clearLayers();
-    console.log(geojson)
-    // re-add the geojson so that it filters out subway lines which do not match state.filter
-    this.state.geojsonLayer.addData(geojson);
-    // fit the map to the new geojson layer's geographic extent
-    this.zoomToFeature(this.state.geojsonLayer);
-  }
 
-  zoomToFeature(target) {
-    // pad fitBounds() so features aren't hidden under the Filter UI element
-    var fitBoundsParams = {
-      paddingTopLeft: [200, 10],
-      paddingBottomRight: [10, 10]
-    };
-    // set the map's center & zoom so that it fits the geographic extent of the layer
-    this.state.map.fitBounds(target.getBounds(), fitBoundsParams);
-  }
-
-  // filterFeatures(feature, layer) {
-  //   // filter the subway entrances based on the map's current search filter
-  //   // returns true only if the filter value matches the value of feature.properties.LINE
-  //   const test = feature.properties.LINE.split('-').indexOf(this.state.subwayLinesFilter);
-  //   if (this.state.subwayLinesFilter === '*' || test !== -1) {
-  //     return true;
-  //   }
-  // }
-
-  pointToLayer(feature, latlng) {
-    // renders our GeoJSON points as circle markers, rather than Leaflet's default image markers
-    // parameters to style the GeoJSON markers
-    var markerParams = {
-      radius: 4,
-      fillColor: 'orange',
-      color: '#fff',
-      weight: 1,
-      opacity: 0.5,
-      fillOpacity: 0.8
-    };
-
-    return L.polygon(latlng, markerParams);
-  }
-
-  onEachFeature(feature, layer) {
-    if (feature.properties && feature.properties.NAME && feature.properties.LINE) {
-
-      // if the array for unique subway line names has not been made, create it
-      // there are 19 unique names total
-      if (subwayLineNames.length < 19) {
-
-        // add subway line name if it doesn't yet exist in the array
-        feature.properties.LINE.split('-').forEach(function (line, index) {
-          if (subwayLineNames.indexOf(line) === -1) subwayLineNames.push(line);
-        });
-
-        // on the last GeoJSON feature
-        if (this.state.geojson.features.indexOf(feature) === this.state.numEntrances - 1) {
-          // use sort() to put our values in alphanumeric order
-          subwayLineNames.sort();
-          // finally add a value to represent all of the subway lines
-          subwayLineNames.unshift('All lines');
-        }
-      }
-
-      // assemble the HTML for the markers' popups (Leaflet's bindPopup method doesn't accept React JSX)
-      const popupContent = `<h3>${feature.properties.NAME}</h3>
-        <strong>Access to MTA lines: </strong>${feature.properties.LINE}`;
-
-      // add our popups
-      layer.bindPopup(popupContent);
-    }
-  }
 
   init(id) {
     if (this.state.map) return;
+    this.addGeoJSONLayer();
+
+
+    // overlayMaps = {
+    // };
     // this function creates the Leaflet map object and is called after the Map component mounts
     let map = L.map(id, config.params);
+    var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
     L.control.zoom({ position: "bottomleft" }).addTo(map);
     L.control.scale({ position: "bottomleft" }).addTo(map);
-    
+
 
     const drawnItems = new L.FeatureGroup();
     map.addLayer(drawnItems);
@@ -271,7 +201,7 @@ class Map extends Component {
       const layer = event.layer;
       drawnItems.addLayer(layer);
       layer.bindPopup("This is a polygon!");
-      
+
     });
 
 
